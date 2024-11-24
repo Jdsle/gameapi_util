2082eaee87ba67843497a941bb00b427e04d26da
Jdsle <Jd1.ext4@gmail.com>
2024-11-23 19:04:57 -0500

    Code cleanup

--- gameapi_util_cpp.py
+++ gameapi_util_cpp.py
@@ -1,24 +1,24 @@
-import urwid
-import gameapi_util_cfg as config
 import os
-from pathlib import Path
 import re
+import urwid
+from pathlib import Path
+import gameapi_util_cfg as config
 
 class gameapi_util:
     def __init__(self):
         self.options = []
         self.selection = 0
-        self.header_text = urwid.Text("gameapi_util (C++)\n", align='left')
-        self.footer_text = urwid.Text("Navigate with Up/Down, Enter to select.", align='left')
-        self.main_body = urwid.SimpleListWalker([])
-        self.main_layout = urwid.Frame(
-            header=urwid.AttrMap(self.header_text, None),
-            body=urwid.ListBox(self.main_body),
-            footer=urwid.AttrMap(self.footer_text, None),
+        self.body = urwid.SimpleListWalker([])
+        header_txt = urwid.Text("gameapi_util (C++)\n", align='left')
+        footer_txt = urwid.Text("Navigate with Up/Down, Enter to select.", align='left')
+        self.layout = urwid.Frame(
+            header=urwid.AttrMap(header_txt, None),
+            body=urwid.ListBox(self.body),
+            footer=urwid.AttrMap(footer_txt, None),
         )
         self.obj_name_field = None
         self.directories = []
-        self.loopState = self.loop_main_menu
+        self.state = self.loop_main_menu
     # __init__ -> (self)
 
     def run(self):
@@ -27,7 +27,7 @@ class gameapi_util:
             ('selected', 'standout', ''),
             ('not_selected', '', '')
         ]
-        loop = urwid.MainLoop(self.main_layout, palette=palette, unhandled_input=lambda key: self.loopState(key))
+        loop = urwid.MainLoop(self.layout, palette=palette, unhandled_input=lambda key: self.state(key))
 
         try:
             loop.run()
@@ -36,7 +36,7 @@ class gameapi_util:
     # run -> (self)
 
     def add_line(self, txt):
-        self.main_body.append(urwid.Text(txt))
+        self.body.append(urwid.Text(txt))
     # add_line -> (self, txt)
 
     def add_option(self, label, onSelectCB):
@@ -46,13 +46,13 @@ class gameapi_util:
     def success_msg_generic(self):
         self.add_line('Done! Press any key to return to the main menu.')
         self.selection = 0
-        self.loopState = self.loop_wait_for_return
+        self.state = self.loop_wait_for_return
     # success_msg_generic -> (self)
 
     def refresh_main_menu(self):
-        self.main_body.clear()
+        self.body.clear()
         for i, option in enumerate(self.options):
-            self.main_body.append(self.update_option(option, selected=(i == self.selection)))
+            self.body.append(self.update_option(option, selected=(i == self.selection)))
     # refresh_main_menu -> (self)
 
     def refresh_obj_dir_menu(self):
@@ -61,7 +61,7 @@ class gameapi_util:
                         'selected' if i == self.selection else 'not_selected')
             for i, dir_name in enumerate(self.directories)]
         )
-        self.main_layout.body = urwid.ListBox(directory_select_walker)
+        self.layout.body = urwid.ListBox(directory_select_walker)
     # refresh_obj_dir_menu -> (self)
 
     def update_option(self, option, selected=False):
@@ -78,7 +78,7 @@ class gameapi_util:
             self.selection = (self.selection + 1) % len(self.options)
             self.refresh_main_menu()
         elif key == 'enter':
-            self.main_body.clear()
+            self.body.clear()
             _label = self.options[self.selection]['label']
             self.add_line(f"> {_label}\n")
             self.options[self.selection]['onSelectCB']()
@@ -89,13 +89,13 @@ class gameapi_util:
     def loop_create_object(self, key):
         if key == 'esc':
             self.selection = 0
-            self.loopState = self.loop_main_menu
+            self.state = self.loop_main_menu
             self.refresh_main_menu()
         elif key == 'enter':
             obj_name = self.obj_name_field.get_edit_text()
             if not obj_name:
                 self.add_line("You're gonna need a name for the object.\n")
-                self.loopState = self.loop_main_menu
+                self.state = self.loop_main_menu
                 self.refresh_main_menu()
                 return
 
@@ -104,15 +104,15 @@ class gameapi_util:
             self.directories = [d for d in os.listdir(config.OBJECT_PATH) if os.path.isdir(os.path.join(config.OBJECT_PATH, d))]
             if not self.directories:
                 self.add_line("\nNo valid object directories found.\n")
-                self.loopState = self.loop_main_menu
+                self.state = self.loop_main_menu
                 self.refresh_main_menu()
                 return
         
             directory_select_walker = urwid.SimpleListWalker([urwid.Text(f"- {dir_name}") for dir_name in self.directories])
-            self.main_layout.body = urwid.ListBox(directory_select_walker)
+            self.layout.body = urwid.ListBox(directory_select_walker)
             self.selection = 0
             self.refresh_obj_dir_menu()
-            self.loopState = self.loop_select_directory
+            self.state = self.loop_select_directory
     # loop_create_object -> (self, key)
 
     def loop_select_directory(self, key):
@@ -124,15 +124,15 @@ class gameapi_util:
             self.refresh_obj_dir_menu()
         elif key == 'enter':
             selected_dir = self.directories[self.selection]
-            obj_dir = os.path.join(config.OBJECT_PATH, selected_dir)
-            codePath = os.path.join(obj_dir, f"{self.obj_name}.cpp")
-            headerPath = os.path.join(obj_dir, f"{self.obj_name}.hpp")
+            object_dir = os.path.join(config.OBJECT_PATH, selected_dir)
+            codePath = os.path.join(object_dir, f"{self.obj_name}.cpp")
+            headerPath = os.path.join(object_dir, f"{self.obj_name}.hpp")
 
             if os.path.exists(codePath) or os.path.exists(headerPath):
                 self.add_line(f"Object '{self.obj_name}' already exists in '{selected_dir}'. Press any key to return to the main menu.")
-                self.main_layout.body = urwid.ListBox(self.main_body)
+                self.layout.body = urwid.ListBox(self.body)
                 self.selection = 0
-                self.loopState = self.loop_wait_for_return
+                self.state = self.loop_wait_for_return
                 return
 
             try:
@@ -230,29 +230,29 @@ class gameapi_util:
                     hpp_out.write(f'}} // namespace {config.OBJECT_NAMESPACE}')
 
                 self.directories.clear()
-                self.main_layout.body = urwid.ListBox(self.main_body)
+                self.layout.body = urwid.ListBox(self.body)
 
                 self.add_line(f"Done! Created '{self.obj_name}' in directory '{selected_dir}'.")
                 self.add_line("Would you like to update the CMake project? (Y/N)")
-                self.loopState = self.loop_create_object_update_prompt
+                self.state = self.loop_create_object_update_prompt
 
             except Exception as e:
                 self.add_line(f"This wasn't supposed to happen... {str(e)}")
-                self.loopState = self.loop_main_menu
+                self.state = self.loop_main_menu
                 self.refresh_main_menu()
     # loop_select_directory -> (self, key)
 
     def loop_create_object_update_prompt(self, key):
         if key in ('y', 'Y'):
             self.project_update()
-            self.loopState = self.loop_main_menu
+            self.state = self.loop_main_menu
         elif key in ('n', 'N'):
             self.add_line("Skipped project update. Press any key to return to the main menu.")
-            self.loopState = self.loop_main_menu
+            self.state = self.loop_main_menu
     # loop_create_object_update_prompt -> (self, key)
 
     def loop_wait_for_return(self, key):
-        self.loopState = self.loop_main_menu
+        self.state = self.loop_main_menu
         self.refresh_main_menu()
     # loop_wait_for_return -> (self, key)
 
@@ -296,10 +296,10 @@ class gameapi_util:
                     self.gen_pub_dns_imp()
                 elif key in ('n', 'N'):
                     self.selection = 0
-                    self.loopState = self.loop_main_menu
+                    self.state = self.loop_main_menu
                     self.refresh_main_menu()
 
-            self.loopState = loop_confirm
+            self.state = loop_confirm
             return
         
         self.gen_pub_dns_imp();
@@ -388,11 +388,11 @@ class gameapi_util:
     # gen_pub_fns_imp -> (self)
 
     def create_object(self):
-        self.loopState = self.loop_create_object
+        self.state = self.loop_create_object
         self.obj_name_field = urwid.Edit("Object Name: ")
 
         input_body = urwid.SimpleListWalker([urwid.AttrMap(self.obj_name_field, None)])
-        self.main_layout.body = urwid.ListBox(input_body)
+        self.layout.body = urwid.ListBox(input_body)
     # create_object -> (self)
 
     def exit_util(self):
@@ -405,7 +405,7 @@ def main():
         app.add_option('Project Update', app.project_update)
         app.add_option('Generate Public Functions (experimental)', app.gen_pub_fns)
         app.add_option('Create Object', app.create_object)
-    config.init(app);
+    config.init(app)
     app.add_option("Exit", app.exit_util)
     app.run()
 # main -> ()
